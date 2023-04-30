b130e4c132ad20842f13d77ba60de93881296858
Rob Amos <rob@robamos.co.uk>
2023-04-30 18:17:32 +0100

    Adding Pre-Commit Linting & Formatting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 
-const app: Express = express();
+let app: Express = express();
 
-app.get('/', (request: Request, response: Response) =>
-  response.send('Hello, World!'),
-);
+app.get('/', (request: Request, response: Response) => response.send('Hello, World!'));
+
 app.listen(8000, () => console.log('Open for business...'));
