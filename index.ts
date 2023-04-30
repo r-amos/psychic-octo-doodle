35cca31709d708295118977d3c45f0df25334d5e
Rob Amos <rob@robamos.co.uk>
2023-04-30 16:57:14 +0100

    Adding Basic Project Boilerplate

---
+++ index.ts
@@ -0,0 +1,8 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+app.get('/', (request: Request, response: Response) =>
+  response.send('Hello, World!')
+);
+app.listen(8000, () => console.log('Open for business...'));
